bc40fced4b9d9d1e7c122487b55fa56dbeaa5f20
Chakradhar-Grandhi <114449662+Chakradhar-Grandhi@users.noreply.github.com>
2022-12-08 18:43:00 -0500

    Role additions

--- src/userinterface/orphanage/ChildCare/ViewCompleteChildDetails.java
+++ src/userinterface/orphanage/ChildCare/ViewCompleteChildDetails.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package userinterface.orphanage.ChildCare;
+package userinterface.Orphanage.ChildCare;
 
 import Business.Child.Child;
 import Business.Child.ChildDirectory;
