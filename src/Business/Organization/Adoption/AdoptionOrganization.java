cac345fae6fec46c027993f5a3672932d6b2e751
NeuYash <pawar.ya@northeastern.edu>
2022-12-07 21:55:45 -0500

    Create AdoptionOrganization.java

---
+++ src/Business/Organization/Adoption/AdoptionOrganization.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization.Adoption;
+
+import Business.Organization.Organization;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yashpawar
+ */
+public class AdoptionOrganization extends Organization{
+
+    public AdoptionOrganization() {
+         super(Organization.Type.Adoption.getValue());
+    
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        return roles;
+    }
+    
+}
+
+
+
+
+   
+    
